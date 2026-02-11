f16ff5615fdb730c1fb5fec05c7c773a49ea0125
SummerMC Developer <developer@summermc.local>
2026-02-12 08:37:49 +0900

    feat: add custom block support and bedrock pack merging

--- components/resource-pack/bedrock-import.ts
+++ components/resource-pack/bedrock-import.ts
@@ -85,6 +85,7 @@ export async function importBedrockPack(
         languages: [],
         particles: [],
         shaders: [],
+        blocks: [],
         packIcon: undefined,
         author: "",
         website: "",
